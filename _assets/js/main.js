5a02cfc2f9cdadc69ad2bdf6543dcb58c2b748b0
Michael Rose <est.michael@gmail.com>
2013-05-06 12:12:00 -0400

    Remove .btn style from jPages gallery pagingation until I can do something better

--- _assets/js/main.js
+++ _assets/js/main.js
@@ -1,8 +1,8 @@
-// @codekit-append "jquery.jPages.js", "jquery.lazyload.js", "jquery.fitvids.js", "picturefill.js", "matchmedia.js";
+// @codekit-append "jquery.jPages.js", "jquery.lazyload.js", "jquery.fitvids.js", "picturefill.js", "matchmedia.js", "jquery.magnific-popup.js";
 
 /*! Plugin options and other jQuery stuff */
 
-// Lazyload
+// Lazyload and j
 $(function() {
 
   /* initiate lazyload defining a custom event to trigger image loading  */
@@ -14,8 +14,8 @@ $(function() {
   /* initiate plugin */
   $("div.holder").jPages({
     containerID: "itemContainer",
-	previous: "←",
-	next: "→",
+	previous: "Previous",
+	next: "Next",
 	perPage: 30,
 	midRange: 3,
 	direction: "random",
@@ -37,18 +37,19 @@ $(function() {
 
 // Table of Contents toggle
 $(function() {
-	$(".toc h3").click(function () {
-		$("#drawer").toggleClass("hidden");
-	});
+  $(".toc h3").click(function () {
+    $("#drawer").toggleClass("hidden");
+  });
 });
+
+// Add lightbox class to all image links
+$("a[href$='.jpg'],a[href$='.png'],a[href$='.gif']").addClass("image-popup");
 
 // Magnific-Popup options
 $(document).ready(function() {
-  $("a[href$='.jpg'],a[href$='.png'],a[href$='.gif']").attr('rel', 'gallery').magnificPopup({
-    delegate: 'a',
+  $('.image-popup').magnificPopup({
     type: 'image',
     tLoading: 'Loading image #%curr%...',
-    mainClass: 'mfp-img-mobile',
     gallery: {
       enabled: true,
       navigateByImgClick: true,
@@ -56,9 +57,10 @@ $(document).ready(function() {
     },
     image: {
       tError: '<a href="%url%">The image #%curr%</a> could not be loaded.',
-      titleSrc: function(item) {
-        return item.el.attr('title') + '<small>by Marsel Van Oosten</small>';
-      }
-    }
+    },
+    removalDelay: 300, // Delay in milliseconds before popup is removed
+    // Class that is added to body when popup is open. 
+    // make it unique to apply your CSS animations just to this exact popup
+    mainClass: 'mfp-fade'
   });
 });
