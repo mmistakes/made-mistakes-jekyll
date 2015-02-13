c5d3b470ea8ffa31728df361a737904ae5e5fe81
Michael Rose <est.michael@gmail.com>
2015-02-13 10:06:38 -0500

    Disable Font Awesome ruby gem - Add fonts

--- _plugins/ext.rb
+++ _plugins/ext.rb
@@ -1,5 +1,5 @@
 require "jekyll-assets"
 require "jekyll-assets/bourbon"
 require "jekyll-assets/neat"
-require "jekyll-assets/font-awesome"
+# require "jekyll-assets/font-awesome"
 require "sprockets"
