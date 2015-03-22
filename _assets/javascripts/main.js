4abef1c31334f9e680c3a1aa693485daba5e4304
Michael Rose <est.michael@gmail.com>
2015-03-21 21:58:44 -0400

    Add back to top button

--- _assets/javascripts/main.js
+++ _assets/javascripts/main.js
@@ -128,3 +128,34 @@ $(".js-social-share").on("click", function(e) {
 $(document).ready(function() {
   $('a').smoothScroll({offset: -20});
 });
+
+
+// Back to top
+jQuery(document).ready(function($){
+  // browser window scroll (in pixels) after which the "back to top" link is shown
+  var offset = 300,
+    //browser window scroll (in pixels) after which the "back to top" link opacity is reduced
+    offset_opacity = 1200,
+    //duration of the top scrolling animation (in ms)
+    scroll_top_duration = 700,
+    //grab the "back to top" link
+    $back_to_top = $('.mm-top');
+
+  //hide or show the "back to top" link
+  $(window).scroll(function(){
+    ( $(this).scrollTop() > offset ) ? $back_to_top.addClass('mm-is-visible') : $back_to_top.removeClass('mm-is-visible mm-fade-out');
+    if( $(this).scrollTop() > offset_opacity ) {
+      $back_to_top.addClass('mm-fade-out');
+    }
+  });
+
+  //smooth scroll to top
+  $back_to_top.on('click', function(event){
+    event.preventDefault();
+    $('body,html').animate({
+      scrollTop: 0 ,
+      }, scroll_top_duration
+    );
+  });
+
+});
