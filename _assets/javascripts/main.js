ef909a24fab7def7e9d03f724c9fbab5e9d18281
Michael Rose <est.michael@gmail.com>
2015-08-27 22:47:24 -0400

    Rough out "• • •" overlay menu

--- _assets/javascripts/main.js
+++ _assets/javascripts/main.js
@@ -3,6 +3,24 @@
 
 $(document).ready(function(){
 
+  // open/close primary navigation
+  $('.cd-primary-nav-trigger').on('click', function(){
+    // $('.cd-menu-icon').toggleClass('is-clicked');
+    // $('.cd-header').toggleClass('menu-is-open');
+
+    // in Firefox transitions break when parent overflow is changed, so we need to wait for the end of the transition to give the body an overflow hidden
+    if( $('.cd-primary-nav').hasClass('is-visible') ) {
+      $('.cd-primary-nav').removeClass('is-visible').one('webkitTransitionEnd otransitionend oTransitionEnd msTransitionEnd transitionend',function(){
+        $('body').removeClass('overflow-hidden');
+      });
+    } else {
+      $('.cd-primary-nav').addClass('is-visible').one('webkitTransitionEnd otransitionend oTransitionEnd msTransitionEnd transitionend',function(){
+        $('body').addClass('overflow-hidden');
+      });
+    }
+  });
+
+
   // Add lightbox class to all image links
   $("a[href$='.jpg'],a[href$='.png'],a[href$='.gif']").addClass("image-popup");
 
