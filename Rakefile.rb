78818974b67d05da08246566e7da21d79731f100
Michael Rose <est.michael@gmail.com>
2015-10-13 10:57:14 -0400

    Update rsync to new grid server

--- Rakefile.rb
+++ Rakefile.rb
@@ -78,7 +78,7 @@ end # task :notify
 desc 'rsync the contents of ./_site to the server'
 task :rsync do
   puts '* rsyncing the contents of ./_site to the server'
-  system 'rsync -prvz --chmod=Du=rwx,Dgo=rx,Fu=rw,Fgo=r _site/ ekoagency.com@s98164.gridserver.com:domains/mademistakes.com/html/'
+  system 'rsync -prvz --chmod=Du=rwx,Dgo=rx,Fu=rw,Fgo=r _site/ new-mademistakes.com@s210904.gridserver.com:domains/mademistakes.com/html/'
 end # task :rsync
 
 # Usage: rake deploy
