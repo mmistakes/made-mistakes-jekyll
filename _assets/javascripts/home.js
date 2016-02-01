a20cf69c9e598898dbf32be08fc3e58378210cb5
Michael Rose <est.michael@gmail.com>
2016-02-01 16:46:31 -0500

    Glitch up the auto typed home page - Add CSS3 animations to text - Add overlay menu and trigger from masthead

--- _assets/javascripts/home.js
+++ _assets/javascripts/home.js
@@ -3,9 +3,43 @@
 
 $(document).ready(function(){
 
+  $("body").addClass("js");
+
+  // toggle overlay navigation
+  $('.overlay__menu-trigger').on('click', function(){
+    // in Firefox transitions break when parent overflow is changed, so we need to wait for the end of the transition to give the body an overflow hidden
+    if( $('.overlay__menu').hasClass('is--visible') ) {
+      $('.overlay__menu').removeClass('is--visible').one('webkitTransitionEnd otransitionend oTransitionEnd msTransitionEnd transitionend');
+      $('#screen').removeClass('is--visible').one('webkitTransitionEnd otransitionend oTransitionEnd msTransitionEnd transitionend');
+    }
+    else {
+      $('.overlay__menu').addClass('is--visible').one('webkitTransitionEnd otransitionend oTransitionEnd msTransitionEnd transitionend');
+      $('#screen').addClass('is--visible').one('webkitTransitionEnd otransitionend oTransitionEnd msTransitionEnd transitionend');
+    }
+  });
+
+  // close overlay navigation on button click
+  $('.overlay__menu-close, #screen').on('click', function(){
+    $('.overlay__menu').removeClass('is--visible').one('webkitTransitionEnd otransitionend oTransitionEnd msTransitionEnd transitionend');
+    $('#screen').removeClass('is--visible').one('webkitTransitionEnd otransitionend oTransitionEnd msTransitionEnd transitionend');
+  });
+
+  // open/close overlay navigation on focus
+  $('.overlay__menu-item a').on('focus', function(){
+    $('.overlay__menu').addClass('is--visible').one('webkitTransitionEnd otransitionend oTransitionEnd msTransitionEnd transitionend');
+    $('#screen').addClass('is--visible').one('webkitTransitionEnd otransitionend oTransitionEnd msTransitionEnd transitionend');
+  });
+  $('.overlay__menu-close').on('focus', function(){
+    $('.overlay__menu').removeClass('is--visible').one('webkitTransitionEnd otransitionend oTransitionEnd msTransitionEnd transitionend');
+    $('#screen').removeClass('is--visible').one('webkitTransitionEnd otransitionend oTransitionEnd msTransitionEnd transitionend');
+  });
+
+  // auto typing
+
   $("#js-home-typed").typed({
-    stringsElement: $("#js-home-strings"),
-    startDelay: 2000,
+    strings: ["<span class='home__title'>Hello^250 my name is&nbsp;M̔̋ͩ̚i̋͢c̛ͪ̈́́̄hͪ̄̆́a̢͋͂ͧē̎̌̿ͣl͋ͬ̽͆͌ͧ̿̕. ^500</span> <span class='br'></span> I am just another boring, ^250 tattooed, ^250 time traveling designer from Buffalo New York.^500 <span class='br'></span> I enjoy eating chicken wings, ^500 <a href='https://mademistakes.com/paperfaces/'>sketching on an iPad</a>, ^500 and playing Xbox.^500 <span class='br'></span> Here you will find a collection of <a href='https://mademistakes.com/articles/'>my writing</a>, ^750 <a href='https://mademistakes.com/mastering-paper/'>Paper by FiftyThree tutorials</a>, ^750 and other <a href='https://mademistakes.com/work/'>creative endeavors</a>. ^250"],
+    contentType: "html",
+    startDelay: 200,
     backDelay: 3000,
     callback: function(){
       home_secondary();
--- index.md
+++ index.md
@@ -6,13 +6,13 @@ excerpt: "Making stuff one mistake at a time."
 ads: false
 ---
 
-<div id="js-home-strings" style="display:none;">
-  <p>
-    <span class="home__title">Hello, my name is Michael ^1500</span><br>
-    I am just another time traveling,^1000 designer, ^1000 husband, ^1000 father of twins, ^1000 illustrator, ^1000 and gadget geek ^1000 from Buffalo New York. ^1000<br>
-    I enjoy eating chicken wings, ^1000 <a href="{{ site.url }}/paperfaces/">sketching on an iPad</a>, ^1000 and playing Xbox. ^1000<br>
-    Made Mistakes is my place for writing <a href="{{ site.url }}/articles/">blog articles</a>, ^1000 <a href="{{ site.url }}/mastering-paper/">Paper by FiftyThree tutorials</a>, ^1500 and other <a href="{{ site.url }}/work/">creative endeavors</a>. ^1000
-  </p>
+<div id="js-home-strings">
+  <h1 class="home__title">Hello, my name is&nbsp;<a href="{{ site.url }}/about/">Michael</a></h1>
+  <div class="home__excerpt">
+    <p>I am just another boring, tattooed, time traveling designer from Buffalo New York.</p>
+    <p>I enjoy eating chicken wings, <a href="{{ site.url }}/paperfaces/">sketching on an iPad</a>, and playing Xbox.</p>
+    <p>Here you will find a collection of <a href="{{ site.url }}/articles/">my writing</a>, <a href="{{ site.url }}/mastering-paper/">Paper by FiftyThree tutorials</a>, and other <a href="{{ site.url }}/work/">creative endeavors</a>.</p>
+  </div>
 </div>
 
 <span id="js-home-typed" class="home__excerpt"></span>
