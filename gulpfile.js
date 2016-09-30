a3bcb8d84acbbe9c60725800e71822aacabaf28b
Michael Rose <est.michael@gmail.com>
2016-09-30 12:40:45 -0400

    Update critical CSS path Gulp tasks

--- gulpfile.js
+++ gulpfile.js
@@ -28,7 +28,7 @@ gulp.task('build', gulp.series('clean', 'assets', 'build:site', 'html'));
 // 'gulp critical' -- builds critical path CSS includes
 // WARNING: run this after substantial CSS changes
 // WARNING: .html files referenced need to exist. run after `gulp build` to ensure.
-gulp.task('critical', gulp.series('critical:article', 'critical:archive'));
+gulp.task('critical', gulp.series('critical:page', 'critical:work', 'critical:archive', 'critical:splash'));
 
 // 'gulp deploy' -- deploy site to production and submit sitemap XML
 gulp.task('deploy', gulp.series('upload', 'submit:sitemap'));
