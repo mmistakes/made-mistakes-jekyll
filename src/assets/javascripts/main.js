2cd0ffb183b6f5c5a8cf420d218de692b2db793f
Michael Rose <est.michael@gmail.com>
2016-10-28 09:31:31 -0400

    Load fonts async on initial load then fallback to cached version for repeat loads

--- src/assets/javascripts/main.js
+++ src/assets/javascripts/main.js
@@ -5,11 +5,13 @@ WebFontConfig = {
   }
 };
 
-(function(d) {
-  var wf = d.createElement('script'), s = d.scripts[0];
-  wf.src = 'https://ajax.googleapis.com/ajax/libs/webfont/1.6.26/webfont.js';
-  s.parentNode.insertBefore(wf, s);
-})(document);
+// if( cookie( fullCSSKey ) ){
+//   (function(d) {
+//     var wf = d.createElement('script'), s = d.scripts[0];
+//     wf.src = 'https://ajax.googleapis.com/ajax/libs/webfont/1.6.26/webfont.js';
+//     s.parentNode.insertBefore(wf, s);
+//   })(document);
+// };
 
 
 $(document).ready(function(){
