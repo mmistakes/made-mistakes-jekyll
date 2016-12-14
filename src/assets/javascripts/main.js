57e0caa486187c9930999773410f8347fdc57b4b
Michael Rose <est.michael@gmail.com>
2016-12-14 12:14:01 -0500

    Add pull-request link to comment success notice

--- src/assets/javascripts/main.js
+++ src/assets/javascripts/main.js
@@ -61,7 +61,7 @@ $(document).ready(function(){
       success: function (data) {
         $('#comment-form-submit').html('Submitted').addClass('btn--disabled');
         $('#comment-form .js-notice').removeClass('notice--danger').addClass('notice--success');
-        showAlert('<strong>Thanks for your comment!</strong> It will show on the site once it has been reviewed and approved.');
+        showAlert('<strong>Thanks for your comment!</strong> It is <a href="https://github.com/mmistakes/made-mistakes-jekyll/pulls">currently pending</a> and will show on the site once approved.');
       },
       error: function (err) {
         console.log(err);
