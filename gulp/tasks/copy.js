405cda4f61c0430c0cf73c2c5d7d376510bd8689
Michael Rose <est.michael@gmail.com>
2017-02-10 15:18:34 -0500

    Update Gulp tasks to properly copy *.ico files to /assets/images/

--- gulp/tasks/copy.js
+++ gulp/tasks/copy.js
@@ -4,14 +4,20 @@ var gulp  = require('gulp');
 // include paths file
 var paths = require('../paths');
 
-// 'gulp assets:copy' -- copies assets into the /dist/ to avoid
-// Jekyll overwriting the whole directory
+// 'gulp copy:assets' -- copies assets into the /dist/
+// to avoid Jekyll overwriting the whole directory
 gulp.task('copy:assets', () =>
-  gulp.src(paths.assetFilesTemp + '/**/*')
+  gulp.src([paths.assetFilesTemp + '/**/*', paths.imageFiles + '/*.ico'])
     .pipe(gulp.dest(paths.assetFilesSite))
 );
 
-// 'gulp jekyll:copy' -- copies processed Jekyll site to /dist/
+// 'gulp copy:icons' -- copies .ico assets to /dist/
+gulp.task('copy:icons', () =>
+  gulp.src(paths.imageFiles + '/*.ico')
+    .pipe(gulp.dest(paths.imageFilesSite))
+);
+
+// 'gulp copy:site' -- copies processed Jekyll site to /dist/
 gulp.task('copy:site', () =>
   gulp.src([paths.tempDir + paths.siteFolderName + '/**/*', paths.tempDir + paths.siteFolderName + '/**/.*'])
     .pipe(gulp.dest(paths.siteFolderName))
