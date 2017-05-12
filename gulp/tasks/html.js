2774fccc82c6cf3ceb38c6d8bc4d58670da4210f
Michael Rose <est.michael@gmail.com>
2017-05-11 21:06:31 -0400

    Adjust critical CSS tasks

--- gulp/tasks/html.js
+++ gulp/tasks/html.js
@@ -58,22 +58,6 @@ var pageDimensions = [{
                         height: 960
                       }];
 
-// 'gulp styles:critical:page' -- extract layout.page critical CSS
-//   into /_includes/critical-page.css
-gulp.task('styles:critical:page', () => {
-  return gulp.src(paths.tempDir  + paths.siteDir + 'articles/ipad-pro/index.html')
-    .pipe(critical({
-      base: paths.tempDir,
-      inline: false,
-      css: [paths.sassFilesTemp + '/style.css'],
-      dimensions: pageDimensions,
-      dest: paths.sourceDir + paths.includesFolderName + '/critical-page.css',
-      minify: true,
-      extract: false,
-      ignore: ['@font-face',/url\(/] // defer loading of webfonts and background images
-    }))
-});
-
 // 'gulp styles:critical:archive' -- extract layout.archive critical CSS
 //   into /_includes/critical-archive.css
 gulp.task('styles:critical:archive', () => {
@@ -81,7 +65,7 @@ gulp.task('styles:critical:archive', () => {
     .pipe(critical({
       base: paths.tempDir,
       inline: false,
-      css: [paths.sassFilesTemp + '/style.css'],
+      css: [paths.sassFilesTemp + '/main.css'],
       dimensions: pageDimensions,
       dest: paths.sourceDir + paths.includesFolderName + '/critical-archive.css',
       minify: true,
@@ -90,31 +74,31 @@ gulp.task('styles:critical:archive', () => {
     }))
 });
 
-// 'gulp styles:critical:work' -- extract layout.work critical CSS
-//   into /_includes/critical-work.css
-gulp.task('styles:critical:work', () => {
-  return gulp.src(paths.tempDir  + paths.siteDir + 'paperfaces/asja-k-portrait/index.html')
+// 'gulp styles:critical:post' -- extract layout.post critical CSS
+//   into /_includes/critical-post.css
+gulp.task('styles:critical:post', () => {
+  return gulp.src(paths.tempDir  + paths.siteDir + 'articles/ipad-pro/index.html')
     .pipe(critical({
       base: paths.tempDir,
       inline: false,
-      css: [paths.sassFilesTemp + '/style.css'],
+      css: [paths.sassFilesTemp + '/main.css'],
       dimensions: pageDimensions,
-      dest: paths.sourceDir + paths.includesFolderName + '/critical-work.css',
+      dest: paths.sourceDir + paths.includesFolderName + '/critical-post.css',
       minify: true,
       extract: false,
       ignore: ['@font-face',/url\(/] // defer loading of webfonts and background images
     }))
 });
 
-// 'gulp styles:critical:splash' -- extract layout.splash critical CSS
-//   into /_includes/critical-splash.css
-gulp.task('styles:critical:splash', () => {
+// 'gulp styles:critical:home' -- extract layout.home critical CSS
+//   into /_includes/critical-home.css
+gulp.task('styles:critical:home', () => {
   return gulp.src(paths.tempDir  + paths.siteDir + 'index.html')
     .pipe(critical({
       base: paths.tempDir,
-      css: [paths.sassFilesTemp + '/style.css'],
+      css: [paths.sassFilesTemp + '/main.css'],
       dimensions: pageDimensions,
-      dest: paths.sourceDir + paths.includesFolderName + '/critical-splash.css',
+      dest: paths.sourceDir + paths.includesFolderName + '/critical-home.css',
       minify: true,
       extract: false,
       ignore: ['@font-face',/url\(/] // defer loading of webfonts and background images
