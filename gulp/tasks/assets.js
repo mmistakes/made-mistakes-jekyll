0e9f1a3c63889c4be3ad2429277f1def483c3f9b
Michael Rose <est.michael@gmail.com>
2018-01-19 08:46:46 -0500

    Enable Autoprefixer grid support

--- gulp/tasks/assets.js
+++ gulp/tasks/assets.js
@@ -1,27 +1,27 @@
 'use strict';
-var argv                 = require('yargs').argv;
-var autoprefixer         = require('autoprefixer');
-var browserSync          = require('browser-sync').create();
-var cheerio              = require('gulp-cheerio');
-var concat               = require('gulp-concat');
-var cssnano              = require('gulp-cssnano');
-var gulp                 = require('gulp');
-var gzip                 = require('gulp-gzip');
-var newer                = require('gulp-newer');
-var postcss              = require('gulp-postcss');
-var rename               = require('gulp-rename');
-var rev                  = require('gulp-rev');
-var revDel               = require('rev-del');
-var sass                 = require('gulp-sass');
-var size                 = require('gulp-size');
-var sourcemaps           = require('gulp-sourcemaps');
-var svgmin               = require('gulp-svgmin');
-var svgstore             = require('gulp-svgstore');
-var uglify               = require('gulp-uglify');
-var when                 = require('gulp-if');
+var argv = require('yargs').argv;
+var autoprefixer = require('autoprefixer');
+var browserSync = require('browser-sync').create();
+var cheerio = require('gulp-cheerio');
+var concat = require('gulp-concat');
+var cssnano = require('gulp-cssnano');
+var gulp = require('gulp');
+var gzip = require('gulp-gzip');
+var newer = require('gulp-newer');
+var postcss = require('gulp-postcss');
+var rename = require('gulp-rename');
+var rev = require('gulp-rev');
+var revDel = require('rev-del');
+var sass = require('gulp-sass');
+var size = require('gulp-size');
+var sourcemaps = require('gulp-sourcemaps');
+var svgmin = require('gulp-svgmin');
+var svgstore = require('gulp-svgstore');
+var uglify = require('gulp-uglify');
+var when = require('gulp-if');
 
 // include paths file
-var paths                = require('../paths');
+var paths = require('../paths');
 
 // 'gulp scripts' -- creates a index.js file with Sourcemap from your JavaScript files
 // 'gulp scripts --prod' -- creates a index.js file from your JavaScript files,
@@ -34,31 +34,31 @@ gulp.task('scripts', () => {
     paths.jsFiles + '/plugins/**/*.js',
     paths.jsFiles + '/main.js'
   ])
-    .pipe(newer(paths.jsFilesTemp + '/index.js', {dest: paths.jsFilesTemp, ext: '.js'}))
+    .pipe(newer(paths.jsFilesTemp + '/index.js', { dest: paths.jsFilesTemp, ext: '.js' }))
     .pipe(when(!argv.prod, sourcemaps.init()))
     // concatenate scripts
     .pipe(concat('index.js'))
-    .pipe(size({showFiles: true}))
+    .pipe(size({ showFiles: true }))
     // minify for production
-    .pipe(when(argv.prod, when('*.js', uglify({preserveComments: 'some'}))))
+    .pipe(when(argv.prod, when('*.js', uglify({ preserveComments: 'some' }))))
     // output sourcemap for development
     .pipe(when(!argv.prod, sourcemaps.write('.')))
     .pipe(gulp.dest(paths.jsFilesTemp))
     // hash JS for production
     .pipe(when(argv.prod, rev()))
-    .pipe(when(argv.prod, size({showFiles: true})))
+    .pipe(when(argv.prod, size({ showFiles: true })))
     // output hashed files
     .pipe(when(argv.prod, gulp.dest(paths.jsFilesTemp)))
     // generate manifest of hashed CSS files
     .pipe(rev.manifest('js-manifest.json'))
     .pipe(gulp.dest(paths.tempDir + paths.sourceDir + paths.dataFolderName))
-    .pipe(when(argv.prod, size({showFiles: true})))
+    .pipe(when(argv.prod, size({ showFiles: true })))
 });
 
 // 'gulp scripts:gzip --prod' -- gzips JS
 gulp.task('scripts:gzip', () => {
   return gulp.src([paths.jsFilesTemp + '/*.js'])
-  .pipe(when(argv.prod, when('*.js', gzip({append: true}))))
+    .pipe(when(argv.prod, when('*.js', gzip({ append: true }))))
     .pipe(when(argv.prod, size({
       gzip: true,
       showFiles: true
@@ -73,44 +73,46 @@ gulp.task('styles', () => {
   return gulp.src([paths.sassFiles + '/main.scss'])
     .pipe(when(!argv.prod, sourcemaps.init()))
     // preprocess Sass
-    .pipe(sass({precision: 10}).on('error', sass.logError))
+    .pipe(sass({ precision: 10 }).on('error', sass.logError))
     // add-remove vendor prefixes
-    .pipe(postcss([ autoprefixer() ]))
+    .pipe(postcss([autoprefixer({
+      grid: true
+    })]))
     // minify for production
-    .pipe(when(argv.prod, when('*.css', cssnano({autoprefixer: false}))))
-    .pipe(size({showFiles: true}))
+    .pipe(when(argv.prod, when('*.css', cssnano({ autoprefixer: false }))))
+    .pipe(size({ showFiles: true }))
     // output sourcemap for development
     .pipe(when(!argv.prod, sourcemaps.write('.')))
     .pipe(when(argv.prod, gulp.dest(paths.sassFilesTemp)))
     // hash CSS for production
     .pipe(when(argv.prod, rev()))
-    .pipe(when(argv.prod, size({showFiles: true})))
+    .pipe(when(argv.prod, size({ showFiles: true })))
     // output hashed files
     .pipe(gulp.dest(paths.sassFilesTemp))
     // generate manifest of hashed CSS files
     .pipe(rev.manifest('css-manifest.json'))
     .pipe(gulp.dest(paths.tempDir + paths.sourceDir + paths.dataFolderName))
-    .pipe(when(argv.prod, size({showFiles: true})))
+    .pipe(when(argv.prod, size({ showFiles: true })))
     .pipe(when(!argv.prod, browserSync.stream()))
 });
 
 // 'gulp styles:gzip --prod' -- gzips CSS
 gulp.task('styles:gzip', () => {
   return gulp.src([paths.sassFilesTemp + '/*.css'])
-    .pipe(when(argv.prod, when('*.css', gzip({append: true}))))
-      .pipe(when(argv.prod, size({
-        gzip: true,
-        showFiles: true
-      })))
-      .pipe(when(argv.prod, gulp.dest(paths.sassFilesTemp)))
+    .pipe(when(argv.prod, when('*.css', gzip({ append: true }))))
+    .pipe(when(argv.prod, size({
+      gzip: true,
+      showFiles: true
+    })))
+    .pipe(when(argv.prod, gulp.dest(paths.sassFilesTemp)))
 });
 
 // 'gulp icons' -- combine all svg icons into single file
 gulp.task('icons', () => {
   return gulp.src(paths.iconFiles + '/*.svg')
     .pipe(svgmin())
-    .pipe(rename({prefix: 'icon-'}))
-    .pipe(svgstore({fileName: 'icons.svg', inlineSvg: true}))
+    .pipe(rename({ prefix: 'icon-' }))
+    .pipe(svgstore({ fileName: 'icons.svg', inlineSvg: true }))
     .pipe(cheerio({
       run: function ($, file) {
         $('svg').attr('style', 'display:none');
