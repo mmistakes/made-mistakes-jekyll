4fdc77154f260e3a9645dc6eda924fc9a673b4aa
Michael Rose <est.michael@gmail.com>
2018-11-08 12:13:37 -0500

    Improve accessibility of BigFoot.js footnotes

--- src/assets/javascripts/main.js
+++ src/assets/javascripts/main.js
@@ -24,7 +24,21 @@ $(document).ready(function() {
   $("a[href$='.jpg'], a[href$='.png'], a[href$='.gif']").attr("data-lity", "");
 
   // Bigfoot footnotes
-  var bigfoot = $.bigfoot();
+  var bigfoot = $.bigfoot({
+    actionOriginalFN: 'delete',
+    buttonMarkup: (
+      '<div class="bigfoot-footnote__container">' +
+      ' <button href="#" class="bigfoot-footnote__button" rel="footnote"' +
+      ' id="{{SUP:data-footnote-backlink-ref}}"' +
+      ' data-footnote-number="{{FOOTNOTENUM}}"' +
+      ' data-footnote-identifier="{{FOOTNOTEID}}"' +
+      ' alt="See Footnote {{FOOTNOTENUM}}"' +
+      ' data-bigfoot-footnote="{{FOOTNOTECONTENT}}">' +
+      ' <span class="visually-hidden">{{FOOTNOTENUM}}</span>' +
+      ' </button>' +
+      '</div>'
+    )
+  });
 });
 
 // Animate sidebar menu items
