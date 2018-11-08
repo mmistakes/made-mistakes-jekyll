05d536332714afffb35c7356ee91dea8556a05dc
Michael Rose <est.michael@gmail.com>
2018-11-08 15:18:05 -0500

    Fix layout for Internet Explorer

--- src/assets/javascripts/main.js
+++ src/assets/javascripts/main.js
@@ -39,6 +39,22 @@ $(document).ready(function() {
       '</div>'
     )
   });
+
+  // Page cover object-fit fix
+  var userAgent, ieReg, ie;
+  userAgent = window.navigator.userAgent;
+  ieReg = /msie|Trident.*rv[ :]*11\./gi;
+  ie = ieReg.test(userAgent);
+
+  if(ie) {
+    $(".page__cover").each(function () {
+      var $container = $(this),
+          imgUrl = $container.find("img").prop("src");
+      if (imgUrl) {
+        $container.css("backgroundImage", 'url(' + imgUrl + ')').addClass("custom-object-fit");
+      }
+    });
+  }
 });
 
 // Animate sidebar menu items
