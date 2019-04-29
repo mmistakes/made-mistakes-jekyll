c0380ead047ddd41bfa64f34b0c7fbea02e84025
Michael Rose <est.michael@gmail.com>
2019-04-29 11:14:06 -0400

    Add `alt` attribute to noscript image

--- src/_plugins/lazyload.rb
+++ src/_plugins/lazyload.rb
@@ -57,7 +57,7 @@ module Jekyll
       }
 
       # Output image tag with everything together
-      "\n<noscript><img src=\"#{image_source}\"></noscript>\n<img #{lqip} #{html_attr_string} class=\"lazyload fade-in\">"
+      "\n<noscript><img src=\"#{image_source}\" #{html_attr_string}></noscript>\n<img #{lqip} #{html_attr_string} class=\"lazyload fade-in\">"
     end
   end
 end
