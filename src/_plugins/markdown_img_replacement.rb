eb672e46ac04580f77a36a56f011ba79fd17da90
Michael Rose <est.michael@gmail.com>
2019-06-10 19:09:15 -0400

    Support all documents
    ref: https://gist.github.com/mmistakes/77c68fbb07731a456805a7b473f47841#gistcomment-2940035

--- src/_plugins/markdown_img_replacement.rb
+++ src/_plugins/markdown_img_replacement.rb
@@ -1,10 +1,10 @@
 # Description: Jekyll plugin to replace Markdown image syntax with lazyload HTML markup
 
-Jekyll::Hooks.register :posts, :pre_render do |post, payload|
-  docExt = post.extname.tr('.', '')
+Jekyll::Hooks.register :documents, :pre_render do |document, payload|
+  docExt = document.extname.tr('.', '')
   # only process if we deal with a markdown file
   if payload['site']['markdown_ext'].include? docExt
-    newContent = post.content.gsub(/(?:!\[(.*?)\]\((.*?)\))/, '<noscript><img src="\2" alt="\1"></noscript><img src="data:image/gif;base64,R0lGODlhAQABAAAAACH5BAEKAAEALAAAAAABAAEAAAICTAEAOw==" data-src="\2" alt="\1" class="lazyload fade-in">')
-    post.content = newContent
+    newContent = document.content.gsub(/(?:!\[(.*?)\]\((.*?)\))/, '<noscript><img src="\2" alt="\1"></noscript><img src="data:image/gif;base64,R0lGODlhAQABAAAAACH5BAEKAAEALAAAAAABAAEAAAICTAEAOw==" data-src="\2" alt="\1" class="lazyload fade-in">')
+    document.content = newContent
   end
 end
